d2e59cabcf40949c9722abe9b44a2eaf07b13969
jduncombe7106 <jduncombe7106@sohamcollege.org.uk>
2014-05-07 09:40:13 +0100

    Update greenBottles.py
    i added a loop so it starts at 10 green bottles but ends at 0, i also added a 1 second sleep between each time a bottle falls off.

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,8 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+
+for x in range(10,0,-1):
+    print("{0} green bottles, hanging on the wall".format(x))
+    print("{0} green bottles, hanging on the wall".format(x))
+    print("And if 1 green bottle should acidentally fall,")
+    print("They'll be {0} green bottles hanging on the wall.".format(x-1))
+    time.sleep(1)
