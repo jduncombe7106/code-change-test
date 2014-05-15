b77d6a53437bf73867a0a062946e37558cd84030
jduncombe7106 <jduncombe7106@sohamcollege.org.uk>
2014-05-15 22:55:57 +0100

    Update linearSearch.py

--- linearSearch.py
+++ linearSearch.py
@@ -5,10 +5,12 @@ lastPosition = -1
 
 
 while testString == "":
-    testString = input("Please enter some text to search : ")
+    testString = raw_input("Please enter some text to search : ")
 
 while len(searchChar) != 1:
-    searchChar = input("Enter a character to search for :")
+    searchChar = raw_input("Enter a character to search for :")
+
+
 
 for x in range (len(testString)):
     if testString[x] == searchChar:
@@ -16,3 +18,15 @@ for x in range (len(testString)):
         lastPosition = x
 
 print ("I found {0} occurences of {1}".format(len(charList),searchChar))
+
+decision=raw_input("do you want to swap a character in that text? Y for yes, N for no: ")
+if decision == ("Y"):
+    change_to = ""
+    while len(change_to) != 1:
+        change_to = raw_input("what letter would you like to change it to? ")
+
+    
+    testString = testString.replace(searchChar,change_to)
+ 
+    print ("The text is now: ",testString)
+    
